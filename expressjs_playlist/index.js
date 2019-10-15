0bd5c548b683dbd725557111a9196557045483bf
Zoleka <zrnkubheka@gmail.com>
2019-10-15 10:54:06 +0200

    Third commit

--- expressjs_playlist/index.js
+++ expressjs_playlist/index.js
@@ -1,16 +1,16 @@
 const express = require('express');
-//const bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 const app = express();
-//const db = require('./queries');
+const db = require('./queries');
 const port = 3090;
-//const Joi = require('joi');
+const Joi = require('joi');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true,
 }));
-
 app.use(express.json());
+
 const musician = [{
     id: 1,
     name: 'Ludacris',
@@ -47,6 +47,8 @@ app.get('/api/musician', (req, res, next) => {
   next();
 });
 
+
+// create a playlist 
 app.post('/api/musician', (req, res) => {
   const {
     error
@@ -62,8 +64,8 @@ app.post('/api/musician', (req, res) => {
 });
 
 app.put('/api/musicians/:id', (req, res) => {
-  const musician = musicians.find(c => c.id === parseInt(req.params.id));
-  if (!musician) return res.status(404).send('The ID was not given');
+  const musician = musicians.find(c => c.name === parseInt(req.params.nae));
+  if (!musician) return res.status(404).send('The name was not given');
 
   const {
     error
@@ -74,6 +76,7 @@ app.put('/api/musicians/:id', (req, res) => {
   res.send(musician);
 });
 
+// delete by Id
 app.delete('/api/musicians/:id', (req, res) => {
   const musician = musicians.find(c => c.id === parseInt(req.params.id));
   if (!musician) return res.status(404).send('The ID was not given');
@@ -85,7 +88,7 @@ app.delete('/api/musicians/:id', (req, res) => {
 });
 
 app.get('/api/musicians/:id', (req, res, next) => {
-  const musician = musicians.find(m => m.id === parseInt(req.params.id));
+  const musician = musicians.find(m => m.name === parseInt(req.params.name));
   if (!musician) return res.status(404).send('The ID was not given');
   res.send(musician);
   next();
@@ -104,92 +107,8 @@ app.listen(port, () => Console.log(`Listerning on port ${port}...`));
 
 
 
-
-
-
-
 app.get('/users', db.getMusicians);
 app.get('/users/:id', db.getMusiciansById);
 app.post('/users', db.createMusicians);
 app.put('/users', db.updateMusicians);
 app.delete('/users/:id', db.deleteMusicians);
-
-app.listen(port, () => {
-  console.log(`Running on port ${port}. lets add it on a db`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-// create a playlist 
-app.post('/api/plalist', (req, res) => {
-  // Create a playlist and return playlist object 
-  resn.send(musicians);
-});
-
-//Getting all the musicians and sort it from A - Z
-app.get('/api/playlist', (req, res, next) => {
-  // To read query string parameters (?sortBy=name)
-  //const sortBy = req.query.sortBy;
-  musicians.sortBy((a, b) => a.name !== b.name ? a.name < b.name ? -1 : 1 : 0);
-
-  // return the musicians
-  res.send(musicians); 
-  next();
-});
-
-// Updating a playlist
-app.put('/api/playlist/:id', (req, res) => {
-
-  // If musicians not found, return 404, otherwise update it
-  // and return the updated object.
-  });
-
-
-// Getting a single musicians
-app.get('/api/playlist/:id', (req, res, next) => {
-  const musiciansId = req.params.id;
-
-  // Lookup the musicians
-  // If not found, return 404
-  res.status(404).send('musician not found.');
-  // Else, return the musicians object
-  res.send(musicians);
-  next();
-  });
-  
-
-
-  // Deleting a musicians
-  app.delete('/api/musicianss/:id', (req, res) => {
-  // If musicians not found, return 404, otherwise delete it
-  // and return the deleted object.
-  });
-
-  // Listen on port 3000
-app.listen(3090, () => console.log('Listening...'));
-
-// Reading the port from an environment variable
-//const port = process.env.PORT || 3000;
-app.listen(port);
-
-*/
-
-/*
-app.get('/', (req, res) => {
-  res.json({
-    info: 'Node.js, Express, and Postgres API, This is killing me slowing'
-  });
-});\
-*/
